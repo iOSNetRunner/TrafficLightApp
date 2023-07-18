58d8cf256440cddc91ceab4022785f9ad1251895
NetRunner <galatskiy@gmail.com>
2023-07-18 12:22:37 +0300

    Initial Commit

---
+++ TrafficLightApp/TrafficLightAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightAppApp.swift
+//  TrafficLightApp
+//
+//  Created by Dmitrii Galatskii on 18.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
